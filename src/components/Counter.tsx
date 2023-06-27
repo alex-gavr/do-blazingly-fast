7fa2f83d804f95cc3ac0c539fe4e615fed131add
Alexander Gavrilenko <sashadrug@mail.ru>
2023-06-27 15:58:53 +0200

    prettier

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -8,15 +8,15 @@ const Counter = ({}: ICounterProps) => {
   const decrement = () => setCount(count - 1);
   const reset = () => setCount(0);
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex flex-col items-center justify-center'>
       <div className={'flex flex-row items-center justify-center gap-4'}>
-        <button className='p-2 bg-slate-200' onClick={increment}>
+        <button className='bg-slate-200 p-2' onClick={increment}>
           +
         </button>
-        <button className='p-2 bg-slate-200' onClick={decrement}>
+        <button className='bg-slate-200 p-2' onClick={decrement}>
           -
         </button>
-        <button className='p-2 bg-slate-200' onClick={reset}>
+        <button className='bg-slate-200 p-2' onClick={reset}>
           Reset
         </button>
       </div>
