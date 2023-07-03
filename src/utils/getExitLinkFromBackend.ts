b25828b2647415b73d6ee6f50b7dc50678fe424f
Alexander Gavrilenko <sashadrug@mail.ru>
2023-07-03 20:17:06 +0200

    remove console dot log

--- src/utils/getExitLinkFromBackend.ts
+++ src/utils/getExitLinkFromBackend.ts
@@ -15,7 +15,6 @@ export const getExitLinkFromBackend = async (ippZone: number) => {
 
   const data = await fetch(url).then((res) => res.json());
   const res = data.ads[0] as Ipp | Onclick;
-  console.log('🚀 ~ res:', res.click);
 
   // onclick response: https://localhost/4292859/?var=&ymid=&b=&campaignid=&osversion=&click_id=&ab2r=&userId=d1c5e65eb3ce42efba09129a5f8567d6
   // ipp response: https://localhost/clicks/mJ4cz5AayejE9t3ZtYdTGKlIuMzxmIlVjwgc6tq213uSnCeS44DXBe683lUwEwSbvln8Gh1V8cQsRn0_I4VU2Ds6cci85qG8nI8RGpmmkdw4XCzEFtaZ0dNmRMz1gDxXnNhFtZpodSkulY3_FJQNV2OaMuzb7ITl4mthfB7tFuhxcUpjefSSIA8HnM9V7-YqcpR9AzFiEEGY0_JjbhrwGTEfRqJH3u2WDBLyAmEHMYc1DGqYwCDU9sGAKnnR9cby0ftgcuCQSojfRt_OTMunwoLmEROoPdh5JBLLbJ_O1QXZAuXjt-x8UFbmMw9_JQV4wJmR4tAfmk-a5HvegVhbQ9tHF3OPLWAmo77ELzcLBCUcCHGYhqOgnhCD9jmoP4AXuRJK-MsNo3cmT4O_guUJP1Yhxb4ykO_9ChNHOjC0PlhclgV7XUQhDf7MgLQJOBzOsLU1HYycexBg2ZZLmlaySHDL2oRYIUEICcjwAsffci5Vl0k07pQZzVIbd9WkRSVEghuHzPZyte0bDNNOj_kmsxBhZlTtLvzY?_z=6020461&b=17917356
