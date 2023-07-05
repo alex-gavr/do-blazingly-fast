df0b6d50dc6685568669834a863296c6df1124f7
Alexander Gavrilenko <sashadrug@mail.ru>
2023-07-05 18:06:47 +0200

    try vercel static instead of edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind()],
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     analytics: true,
   }),
