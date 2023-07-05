8aea0b491fd61b91558ffe9911db268320e78eb5
Alexander Gavrilenko <sashadrug@mail.ru>
2023-07-05 20:26:32 +0200

    go back to edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind()],
-  output: 'static',
+  output: 'server',
   adapter: vercel({
     analytics: true,
   }),
