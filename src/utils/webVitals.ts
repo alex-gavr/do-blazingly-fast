a3e6aa669f30fcfb45aeea93a8492981e8a89f30
Alexander Gavrilenko <sashadrug@mail.ru>
2023-07-15 18:51:44 +0200

    make web vitals faster

--- src/utils/webVitals.ts
+++ src/utils/webVitals.ts
@@ -1,30 +1,4 @@
-import { Metric, onLCP, onFCP, onTTFB, onFID, onINP, onCLS } from 'web-vitals';
-import production from '@utils/isProduction';
-
-let debug: boolean = false;
-if (typeof window !== 'undefined') {
-  const url = new URL(window.location.href);
-  const geo = url.searchParams.get('geo') ?? '??';
-  const offer = url.searchParams.get('offer_id') ?? '??';
-  const pathname = url.pathname;
-  debug = url.searchParams.get('debug') ? true : false;
-
-  if (production && !debug) {
-    // Largest Contentful Paint (LCP)
-    onLCP((metric) => sendWebVitals({ metric, geo, pathname, offer }));
-    // First Contentful Paint (FCP)
-    onFCP((metric) => sendWebVitals({ metric, geo, pathname, offer }));
-    // Time to First Byte (TTFB)
-    onTTFB((metric) => sendWebVitals({ metric, geo, pathname, offer }));
-    // Cumulative Layout Shift (CLS) | IDC
-    onCLS((metric) => sendWebVitals({ metric, geo, pathname, offer }));
-    // First Input Delay (FID) | IDC
-    onFID((metric) => sendWebVitals({ metric, geo, pathname, offer }));
-    // Interaction to next Paint (INP) | IDC
-    onINP((metric) => sendWebVitals({ metric, geo, pathname, offer }));
-  }
-}
-
+import type { Metric } from 'web-vitals';
 interface IProps {
   metric: Metric;
   geo: string;
@@ -32,7 +6,7 @@ interface IProps {
   offer: string;
 }
 
-function sendWebVitals({ metric, geo, pathname, offer }: IProps) {
+export default async function sendWebVitals({ metric, geo, pathname, offer }: IProps) {
   const data = {
     id: metric.id,
     geo: geo,
