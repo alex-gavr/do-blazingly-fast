f86beb007875ed6dd12dc9ed7278da03e3b5aff4
Alexander Gavrilenko <sashadrug@mail.ru>
2023-07-24 16:56:16 +0200

    try handling swipes

---
+++ src/components/Monetization/EventListeners.tsx
@@ -0,0 +1,47 @@
+import debug from '@src/utils/isDebug';
+import { useEventListener } from '@src/utils/useEventListener';
+
+interface IEventListenersProps {}
+
+const EventListeners = ({}: IEventListenersProps) => {
+  const pushState = () => {
+    const url = new URL(window.location.href);
+    console.log('i ran');
+    // const pathname = url.pathname;
+    const searchParams = url.searchParams;
+    for (let index = 0; index <= 3; index++) {
+      history.pushState(null, 'Finance Survey', `/${searchParams}`);
+    }
+  };
+
+  const handleBackButton = async (event: PopStateEvent) => {
+    event.preventDefault();
+    if (!debug) {
+      const { financeExitsState } = await import('@context/state');
+      const { default: makeExitUrl, ExitType } = await import('@src/utils/makeExitUrl');
+      const { getRandomZone } = await import('@src/utils/getRandomZone');
+      const { initBack } = await import('./Back');
+
+      const financeExits = financeExitsState.get();
+
+      const zone = getRandomZone(financeExits.onclick_reverse_zone);
+      const url = makeExitUrl(zone, ExitType.onclick);
+
+      initBack(financeExits.onclick_back_zone);
+      window.location.replace(url);
+    } else {
+      console.log('reverse is not available in debug mode');
+    }
+  };
+
+  useEventListener('scroll', pushState);
+  useEventListener('touchstart', pushState);
+  useEventListener('touchend', pushState);
+  useEventListener('touchcancel', pushState);
+  useEventListener('click', pushState);
+  useEventListener('popstate', handleBackButton);
+
+  return null;
+};
+
+export default EventListeners;
