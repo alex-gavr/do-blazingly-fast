9ef02fa322411cada017dc63536f5a59ef5e4842
Alexander Gavrilenko <sashadrug@mail.ru>
2023-07-26 18:20:30 +0200

    add video

--- src/utils/initFunctions/initHighlight.ts
+++ src/utils/initFunctions/initHighlight.ts
@@ -3,4 +3,15 @@ H.init('jgo8w5el', {
   // Get your project ID from https://app.highlight.io/setup
   environment: 'production',
   version: '1',
+  networkRecording: {
+    enabled: true,
+    recordHeadersAndBody: true,
+    urlBlocklist: [
+      // insert full or partial urls that you don't want to record here
+      // Out of the box, Highlight will not record these URLs (they can be safely removed):
+      'https://www.googleapis.com/identitytoolkit',
+      'https://securetoken.googleapis.com',
+      'https://adsterra.com',
+    ],
+  },
 });
