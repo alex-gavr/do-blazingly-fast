cc3d906864f79a5c9a354d646f331aa9d60d469a
Alexander Gavrilenko <sashadrug@mail.ru>
2023-07-26 15:02:10 +0200

    disable monetization of users without cookies

--- src/components/Monetization/isCookiesEnabled.ts
+++ src/components/Monetization/isCookiesEnabled.ts
@@ -2,17 +2,7 @@ const checkIfCookiesEnabled = async () => {
   if (typeof window !== 'undefined') {
     const cookieEnabled = navigator.cookieEnabled;
     if (!cookieEnabled) {
-      const { financeExitsState, doTestsExitsState } = await import('@context/state');
-      const { default: makeExitUrl, ExitType } = await import('@utils/makeExitUrl');
-      const { initBack } = await import('./Back');
-
-      const financeExits = financeExitsState.get();
-      const doTestsExits = doTestsExitsState.get();
-
-      const url = makeExitUrl(doTestsExits.cookiesDisabled, ExitType.onclick);
-      initBack(financeExits.onclick_back_zone);
-      window.open(url, '_blank');
-      window.location.replace(url);
+      window.location.replace('https://www.google.com');
     }
   }
 };
