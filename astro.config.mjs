721d469cb84d7c5611de05c68860528ea2c24e1e
Alexander Gavrilenko <sashadrug@mail.ru>
2023-09-17 14:39:58 +0200

    Batman

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind()],
   output: 'server',
   adapter: vercel({
-    edgeMiddleware: true,
+    imageService: true,
   }),
   site: 'https://blazingly-fast-do.top/',
 });
