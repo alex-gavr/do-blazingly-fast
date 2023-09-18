fc1d61694c8d6bf44a879699123461cb8d670ff1
Alexander Gavrilenko <sashadrug@mail.ru>
2023-09-18 20:13:34 +0200

    conversions under 5 seconds are not conversion

--- src/utils/doConversion.ts
+++ src/utils/doConversion.ts
@@ -6,18 +6,31 @@ import production from './simpleFunctions/isProduction';
 
 const doConversion = () => {
   if (typeof window !== 'undefined') {
-    if (production) {
-      const url = new URL(window.location.href);
-      const subId = url.searchParams.get('s');
-      const conversionUrl = `${import.meta.env.PUBLIC_CONVERSION_URL}${subId}`;
-      if (navigator.sendBeacon) {
-        navigator.sendBeacon(conversionUrl);
+    const startTime = sessionStorage.getItem('surveyStartTime');
+    if (startTime) {
+      const timeTaken = (Date.now() - Number(startTime)) / 1000;
+
+      if (timeTaken < 5) {
+        justLog({ somethingToLog: 'Survey completed too quickly, not triggering conversion.', type: 'log' });
+        sessionStorage.removeItem('surveyStartTime');
+        return; // Exit the function early, not triggering conversion
       } else {
-        fetch(conversionUrl, { method: 'POST', keepalive: true });
+        justLog({ somethingToLog: 'Survey completed in reasonable time, triggering conversion.', type: 'log' });
+        if (production) {
+          const url = new URL(window.location.href);
+          const subId = url.searchParams.get('s');
+          const conversionUrl = `${import.meta.env.PUBLIC_CONVERSION_URL}${subId}`;
+          if (navigator.sendBeacon) {
+            navigator.sendBeacon(conversionUrl);
+          } else {
+            fetch(conversionUrl, { method: 'POST', keepalive: true });
+          }
+          production && !debug && setCookie('nonUnique', 'true', { expires: 7, path: '' });
+        } else {
+          justLog({ somethingToLog: 'conversion is fired', type: 'log' });
+        }
+        sessionStorage.removeItem('surveyStartTime');
       }
-      production && !debug && setCookie('nonUnique', 'true', { expires: 7, path: '' });
-    } else {
-      justLog({ somethingToLog: 'conversion is fired', type: 'log' });
     }
   } else {
     throw new Error('window is undefined');
