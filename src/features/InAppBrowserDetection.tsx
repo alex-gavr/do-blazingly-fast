35c7ec360e7bd6d173fbed9ad44bbc448e98e2d6
Alexander Gavrilenko <sashadrug@mail.ru>
2023-10-14 17:37:41 +0200

    batman

--- src/features/InAppBrowserDetection.tsx
+++ src/features/InAppBrowserDetection.tsx
@@ -28,8 +28,8 @@ const InAppBrowserDetection = () => {
         position: 'fixed',
         top: 0,
         left: 0,
-        width: '100%',
-        height: '100%',
+        bottom: 0,
+        right: 0,
         zIndex: 9999,
       }}
       onClick={handleOverlayClick}
