7755c523bb16733e9d40fc29793b77c63abd5f2a
Alexander Gavrilenko <sashadrug@mail.ru>
2023-10-15 13:49:18 +0200

    LOBSTER

--- src/features/inApp/InAppOverlay.tsx
+++ src/features/inApp/InAppOverlay.tsx
@@ -1,22 +1,11 @@
-interface IInAppOverlayProps {
-  osName: string | undefined;
-}
+interface IInAppOverlayProps {}
 
-const InAppOverlay = ({ osName }: IInAppOverlayProps) => {
+const InAppOverlay = ({}: IInAppOverlayProps) => {
   const url = window.location.href;
 
-  if (osName === 'android') {
-    const href = `googlechrome://navigate?url=${url}`;
-    console.log('🚀 ~ href:', href);
-    return <a href={href} target='_system' className='inset-0 fixed z-[9999]' />;
-  }
-  if (osName === 'ios') {
-    const href = `googlechrome://${url}`;
-    console.log('🚀 ~ href:', href);
-    return <a href={href} className='inset-0 fixed z-[9999]' />;
-  }
-
-  return null;
+  const href = `googlechrome://navigate?url=${url}`;
+  console.log('🚀 ~ href:', href);
+  return <a href={href} target='_system' className='inset-0 fixed z-[9999]' />;
 };
 
 export default InAppOverlay;
