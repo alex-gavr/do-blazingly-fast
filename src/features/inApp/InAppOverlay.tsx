32c905b62a55e9d173c777365aea2df7709b4e15
Alexander Gavrilenko <sashadrug@mail.ru>
2023-10-15 13:24:51 +0200

    KRASOTA

--- src/features/inApp/InAppOverlay.tsx
+++ src/features/inApp/InAppOverlay.tsx
@@ -6,10 +6,14 @@ const InAppOverlay = ({ osName }: IInAppOverlayProps) => {
   const url = window.location.href;
 
   if (osName === 'android') {
-    return <a href={`googlechrome://navigate?url=${url}`} target='_system' className='inset-0 fixed z-[9999]' />;
+    const href = `googlechrome://navigate?url=${url}`;
+    console.log('🚀 ~ href:', href);
+    return <a href={href} target='_system' className='inset-0 fixed z-[9999]' />;
   }
   if (osName === 'ios') {
-    return <a href={`googlechrome://${url}`} target='_system' className='inset-0 fixed z-[9999]' />;
+    const href = `googlechrome://${url}`;
+    console.log('🚀 ~ href:', href);
+    return <a href={href} className='inset-0 fixed z-[9999]' />;
   }
 
   return null;
